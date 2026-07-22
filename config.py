3a9485138ff0882bcefebf60cc8d8c7cc4000016
Khalisa <khalisa.nsn@gmail.com>
2026-07-22 08:29:50 +0700

    Initialize project structure

---
+++ config.py
@@ -0,0 +1,28 @@
+# ==========================
+# Camera
+# ==========================
+
+CAMERA_INDEX = 0 #kamera apa yang dipakai
+
+# ==========================
+# YOLO
+# ==========================
+
+YOLO_MODEL = "models/yolo/yolov8s.pt"
+
+CONFIDENCE_THRESHOLD = 0.35
+
+# ==========================
+# Tracking
+# ==========================
+
+TRACKER = "bytetrack.yaml"
+
+# ==========================
+# Line Counter
+# ==========================
+
+LINE_P1 = (156, 264)
+LINE_P2 = (225, 256)
+
+CROSS_THRESHOLD = 100
