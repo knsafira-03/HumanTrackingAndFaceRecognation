1ca8fdeb95b2a743cedd59f621917e4a362f5820
Khalisa <khalisa.nsn@gmail.com>
2026-07-22 09:27:26 +0700

    Sprint 1: Add live camera module

--- app/detector/detector.py
+++ app/detector/detector.py
@@ -1,28 +1,39 @@
 from ultralytics import YOLO
 import cv2
-import os
 
 
 class PersonDetector:
 
     def __init__(self, model_path):
 
+        print("[INFO] Loading YOLO...")
         self.model = YOLO(model_path)
+        print("[INFO] YOLO Loaded")
 
         self.cap = None
 
     def open_camera(self, camera_index):
+
+        print("[INFO] Opening Camera...")
 
         self.cap = cv2.VideoCapture(camera_index)
 
         if not self.cap.isOpened():
             raise Exception("Camera gagal dibuka.")
 
+        print("[INFO] Camera Opened")
+
     def read_frame(self):
 
         return self.cap.read()
+
+    def show_frame(self, frame, window_name):
+
+        cv2.imshow(window_name, frame)
 
     def release(self):
 
         if self.cap is not None:
             self.cap.release()
+
+        cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 from app.detector.detector import PersonDetector
 from config import *
 
+
 def main():
 
     print("====================================")
     print(" Human Monitoring System")
-    print(" Sprint 1 - Detector Module")
+    print(" Sprint 1 - Live Camera")
     print("====================================")
 
     detector = PersonDetector(YOLO_MODEL)
 
     detector.open_camera(CAMERA_INDEX)
 
+    while True:
+
+        ret, frame = detector.read_frame()
+
+        if not ret:
+            print("Frame tidak terbaca")
+            break
+
+        detector.show_frame(frame, WINDOW_NAME)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+
     detector.release()
 
-    print("\nDetector Module Success")
+    print("Program selesai")
+
 
 if __name__ == "__main__":
+    import cv2
     main()
